9189fe4750627a64504c0d6e609b70355415cd19
GunawanMayo <Qwanser3301@gmail.com>
2025-05-08 23:27:49 +0900

    mysql

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mysql2 = require("mysql2");
 
 const app = express();
 
@@ -10,12 +11,51 @@ const server = app.listen(3000, function () {
 app.disable("x-powered-by")
 app.use(cors()).use(express.json());
 
+const connection = mysql2.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "user",
+  password: "password",
+  database: "sample"
+});
+
+connection.connect(function (error) {
+  if (error) {
+    console.log(`failed to connect to mysql: ${error}`);
+    throw error;
+  }
+  console.log("connect mysql:");
+});
+
 app.get('/', (req, res) => {
-  res.json({
-    is: "1",
-    name: "hogehoge",
-    description: "hello world",
+  const sql = `
+    select 
+      id, 
+      title, 
+      description 
+    from 
+      todos
+  `
+  connection.query(sql, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.json(result);
   });
 });
 
-console.log("hello world");
+app.post("/api/todos", (req, res) => {
+  const todo = req.body;
+
+  const sql = `
+    insert into todos (title,description)
+    values ("${todo.title}", "${todo.description}")
+  `
+
+  connection.query(sql, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.status(201).json(result.insertId);
+  })
+});
